06ccb1300898b822be64b746b3d1207e0dd9f5e8
quanex1 <62199007+quanex1@users.noreply.github.com>
2021-06-15 21:39:14 +0300

    Update main.cpp
    no border

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QStatusBar>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint);
+    w.statusBar()->hide(); // for bottom bar
     w.show();
 
     return a.exec();
